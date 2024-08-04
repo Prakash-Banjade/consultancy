97f0091b984f35fb12bfc7c22656cef273434a4f
Prakash Banjade <prakashbanjade191@gmail.com>
2024-08-04 12:34:32 +0545

    worked on psersonal information

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,13 +1,20 @@
 import { BaseEntity } from "src/core/entities/base.entity";
-import { ECountry } from "src/core/types/countries.type";
-import { EGender, EMaritalStatus } from "src/core/types/global.types";
-import { File } from "src/files/entities/file.entity";
 import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
+import { PersonalInfo } from "../personal-infos/entities/personal-info.entity";
 
 @Entity()
 export class Student extends BaseEntity {
+    /**
+    |--------------------------------------------------
+    | PRIMARY DETAILS
+    |--------------------------------------------------
+    */
+
     @Column({ type: 'varchar' })
     firstName: string;
+
+    @Column({ type: 'varchar' })
+    middleName: string;
 
     @Column({ type: 'varchar' })
     lastName: string;
@@ -15,95 +22,20 @@ export class Student extends BaseEntity {
     @Column({ type: 'varchar' })
     email: string;
 
-    @Column({ type: 'datetime' })
-    dob: string;
-
-    @Column({ type: 'varchar' })
-    firstLanguage: string;
-
-    @Column({ type: 'enum', enum: ECountry })
-    countryOfCitizenship: ECountry;
-
-    @Column({ type: 'varchar' })
-    passportNumber: string;
-
-    @Column({ type: 'datetime' })
-    passportExpiry: string;
-
-    @Column({ type: 'enum', enum: EGender })
-    gender: EGender
-
-    @Column({ type: 'enum', enum: EMaritalStatus })
-    maritalStatus: EMaritalStatus
-
     @Column({ type: 'varchar' })
     phoneNumber: string;
 
-    @Column({ type: 'int' })
-    postalCode: number;
+    @Column({ type: 'datetime' })
+    dob: string;
 
-    @Column({ type: 'varchar' })
-    address: string;
-
-    @Column({ type: 'varchar' })
-    city: string;
-
-    @Column({ type: 'varchar' })
-    emergencyContactName: string;
-
-    @Column({ type: 'varchar' })
-    emergencyContactNumber: string;
-
-    @Column({ type: 'varchar' })
-    emergencyContactEmail: string;
-
-    /**
-    |--------------------------------------------------
-    | DOCUMENTS
-    |--------------------------------------------------
-    */
-
-    @OneToOne(() => File)
+    // PERSONAL INFO
+    @OneToOne(() => PersonalInfo, { nullable: true })
     @JoinColumn()
-    cv: File;
+    personalInfo: PersonalInfo;
 
-    @OneToOne(() => File)
-    @JoinColumn()
-    recommendationLetter: File;
+    // ACADEMIC QUALIFICATION
 
-    @OneToOne(() => File)
-    @JoinColumn()
-    experienceDoc: File;
+    // WORK EXPERIENCE
 
-    @OneToOne(() => File)
-    @JoinColumn()
-    passportDoc: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    ieltsPteMarkDoc: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    certificate_grade_10: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    transcript_grade_10: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    certificate_plus_two: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    transcript_plus_two: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    certificate_bachelor: File;
-
-    @OneToOne(() => File)
-    @JoinColumn()
-    transcript_bachelor: File;
+    // TESTS
 }
---
+++ src/students/personal-infos/entities/personal-info.entity.ts
@@ -0,0 +1,115 @@
+import { Address } from "src/addresses/entities/address.entity";
+import { BaseEntity } from "src/core/entities/base.entity";
+import { ECountry } from "src/core/types/countries.type";
+import { EGender, EMaritalStatus } from "src/core/types/global.types";
+import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
+
+@Entity()
+export class PersonalInfo extends BaseEntity {
+    /**
+    |--------------------------------------------------
+    | PERSONAL INFO
+    |--------------------------------------------------
+    */
+
+    @Column({ type: 'datetime' })
+    dob: string;
+
+    @Column({ type: 'enum', enum: EGender })
+    gender: EGender
+
+    @Column({ type: 'enum', enum: EMaritalStatus })
+    maritalStatus: EMaritalStatus
+
+    /**
+    |--------------------------------------------------
+    | ADDRESS INFO
+    |--------------------------------------------------
+    */
+
+    @OneToOne(() => Address)
+    @JoinColumn({ name: 'mailingAddressId' })
+    mailingAddress: Address
+
+    @OneToOne(() => Address)
+    @JoinColumn({ name: 'permanentAddressId' })
+    permanentAddress: Address
+
+    /**
+    |--------------------------------------------------
+    | PASSPORT INFO
+    |--------------------------------------------------
+    */
+
+    @Column({ type: 'varchar' })
+    passportNumber: string
+
+    @Column({ type: 'datetime' })
+    passportIssueDate: string
+
+    @Column({ type: 'datetime' })
+    passportExpiryDate: string
+
+    @Column({ type: 'enum', enum: ECountry, name: 'passportIssueCountry' })
+    passportIssueCountry: ECountry
+
+    @Column({ type: 'varchar' })
+    cityOfBirth: string
+
+    @Column({ type: 'enum', enum: ECountry })
+    countryOfBirth: ECountry;
+
+    /**
+    |--------------------------------------------------
+    | NATIONALITY
+    |--------------------------------------------------
+    */
+
+    @Column({ type: 'enum', enum: ECountry })
+    nationality: ECountry;
+
+    @Column({ type: 'enum', enum: ECountry })
+    citizenship: ECountry;
+
+    @Column({ type: 'enum', enum: ECountry, name: 'livingAndStudyingCountry', nullable: true })
+    livingAndStudyingCountry: ECountry
+
+    @Column({ type: 'simple-array', nullable: true })
+    otherCountrysCitizens: string[]
+
+    /**
+    |--------------------------------------------------
+    | BACKGROUND INFO
+    |--------------------------------------------------
+    */
+
+    @Column({ type: 'enum', enum: ECountry, name: 'appliedImmigrationCountry', nullable: true })
+    appliedImmigrationCountry: ECountry
+
+    @Column({ type: 'varchar', nullable: true })
+    medicalCondition: string
+
+    @Column({ type: 'varchar', nullable: true })
+    visaRefusalCountries: string; // format: [{country: ECountry, visaType: string}]
+
+    @Column({ type: 'varchar', nullable: true })
+    criminalRecord: string;
+
+    /**
+    |--------------------------------------------------
+    | IMPORTANT CONTACTS
+    |--------------------------------------------------
+    */
+
+    @Column({ type: 'varchar' })
+    emergencyContactName: string
+
+    @Column({ type: 'varchar' })
+    emergencyContactPhoneNumber: string
+
+    @Column({ type: 'varchar' })
+    emergencyContactEmail: string
+
+    @Column({ type: 'varchar' })
+    relationWithApplicant: string;
+}

379a08b00591496124edac3aad703e849c2a0729
Prakash Banjade <prakashbanjade191@gmail.com>
2024-08-06 15:23:42 +0545

    add query search in student

--- src/students/dto/student-query.dto.ts
+++ src/students/dto/student-query.dto.ts
@@ -2,6 +2,8 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsOptional, IsString } from "class-validator";
 import { QueryDto } from "src/core/dto/query.dto";
+import { EApplicationStatus } from "src/core/types/global.types";
+import { EMonth } from "src/core/types/months.types";
 
 export class StudentQueryDto extends QueryDto {
     @ApiPropertyOptional({ format: 'date-time' })
@@ -14,8 +16,25 @@ export class StudentQueryDto extends QueryDto {
     @IsOptional()
     createdTo: string;
 
+    @ApiPropertyOptional({ enum: EMonth })
+    @IsOptional()
+    @Transform(({ value }) => value.split(',')) // format: 'january,february,march'
+    intakeMonths: string[];
+
     @ApiPropertyOptional()
-    @IsString()
+    @IsOptional()
+    @Transform(({ value }) => {
+        const yearArr = value?.split(',')?.map((year: string) => isNaN(Number(year)) ? null : Number(year))?.filter(Boolean);
+        return (Array.isArray(yearArr) && yearArr.length > 0) ? yearArr : [];
+    })
+    intakeYears: number[]
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @Transform(({ value }) => value.split(','))
+    statuses: EApplicationStatus[];
+
+    @ApiPropertyOptional()
     @Transform(({ value }) => value.split(','))
     @IsOptional()
     countryIds: string[]

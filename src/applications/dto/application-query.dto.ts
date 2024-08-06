7581b3f53bb90c11a3ecf8325f4332615bde6654
Prakash Banjade <prakashbanjade191@gmail.com>
2024-08-06 15:00:52 +0545

    add search query for application

--- src/applications/dto/application-query.dto.ts
+++ src/applications/dto/application-query.dto.ts
@@ -1,5 +1,60 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsOptional, IsString } from "class-validator";
 import { QueryDto } from "src/core/dto/query.dto";
+import { ECountry } from "src/core/types/countries.type";
+import { EApplicationStatus } from "src/core/types/global.types";
+import { EMonth } from "src/core/types/months.types";
 
 export class ApplicationQueryDto extends QueryDto {
-    
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    createdAt: string;
+
+    @ApiPropertyOptional({ enum: ECountry })
+    @IsString()
+    @IsOptional()
+    country: string;
+
+    @ApiPropertyOptional({ format: 'uuid' })
+    @IsString()
+    @IsOptional()
+    universityId: string;
+
+    @ApiPropertyOptional({ enum: EMonth })
+    @IsString()
+    @IsOptional()
+    @Transform(({ value }) => value.split(',')) // format: 'january,february,march'
+    intakeMonths: string[];
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @Transform(({ value }) => {
+        const yearArr = value?.split(',')?.map((year: string) => isNaN(Number(year)) ? null : Number(year))?.filter(Boolean);
+        return (Array.isArray(yearArr) && yearArr.length > 0) ? yearArr : [];
+    })
+    intakeYears: number[]
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @Transform(({ value }) => value.split(','))
+    statuses: EApplicationStatus[];
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    courseName: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    studentName: string;
+
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    ackNo: string;
 }
